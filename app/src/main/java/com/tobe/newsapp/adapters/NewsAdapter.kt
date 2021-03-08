6f36b475d5ee9eb87961b87adaae5f795b827bd5
IHEME TOBECHUKWU <ihemeprogress@gmail.com>
2021-03-08 14:08:50 +0100

    Added Recycler Views

--- app/src/main/java/com/tobe/newsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/tobe/newsapp/adapters/NewsAdapter.kt
@@ -1,4 +1,66 @@
 package com.tobe.newsapp.adapters
 
-class NewsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.tobe.newsapp.data.models.Article
+import com.tobe.newsapp.databinding.ItemNewsBinding
+import kotlinx.android.synthetic.main.item_news.view.*
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(private val binding: ItemNewsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    }
+
+    private val differCallBack = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    val differ = AsyncListDiffer(this@NewsAdapter, differCallBack)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val binding = ItemNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ArticleViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this)
+                .load(article.url)
+                .into(ivArticleImage)
+
+            tvSource.text = article.source.name
+            tvDescription.text = article.description
+            tvTitle.text = article.title
+            tvPublishedAt.text = article.publishedAt
+
+            setOnClickListener {
+                onItemClickListener?.let { it(article) }
+            }
+        }
+
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
 }
